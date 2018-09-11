96e8ab4c03fa9d21bed7d20bbdcfb5e894b60e61
Josh Shuster <josh.shuster@wpengine.com>
2018-09-11 10:00:12 -0500

    Added .go to polly file

---
+++ pkg/speakers/polly/polly.go
@@ -0,0 +1,84 @@
+package polly
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/polly"
+
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	defaultVoice = "joanna"
+)
+
+var (
+	// acceptableVoices is the set of voices currently supported by SGTM. More AWS Polly voices can be found here: https://docs.aws.amazon.com/polly/latest/dg/voicelist.html.
+	acceptableVoices = map[string]*string{
+		defaultVoice: aws.String("Joanna"),
+		"joey":       aws.String("Joey"),
+		"justin":     aws.String("Justin"),
+		"matthew":    aws.String("Matthew"),
+		"ivy":        aws.String("Ivy"),
+		"kendra":     aws.String("Kendra"),
+		"kimberly":   aws.String("Kimberly"),
+		"salli":      aws.String("Salli"),
+	}
+)
+
+type PollySpeaker struct {
+	*polly.Polly
+	VoiceID *string
+}
+
+// New creates an AWS Polly session. It takes in a voiceName string to determine which speaker to use.
+func New(voiceName string) *PollySpeaker {
+	var voice *string
+	voice, ok := acceptableVoices[voiceName]
+	if !ok {
+		voice = acceptableVoices[defaultVoice]
+	}
+
+	// Initialize a session that the SDK uses to load
+	// credentials from the shared credentials file. (~/.aws/credentials).
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	return &PollySpeaker{
+		Polly:   polly.New(sess),
+		VoiceID: voice,
+	}
+}
+
+// GenerateSpeech takes in a string and outputs Synthesized Speech and possible an error.
+func (ps *PollySpeaker) GenerateSpeech(s string, fileName string) error {
+	input := &polly.SynthesizeSpeechInput{OutputFormat: aws.String("mp3"), Text: aws.String(s), VoiceId: ps.VoiceID}
+	output, err := ps.SynthesizeSpeech(input)
+	if err != nil {
+		return err
+	}
+	return ps.saveAsMP3(fileName, output)
+}
+
+// SaveAsMP3 takes a file name and the synthesized speech from GenerateSpeech and saves a MP3 file of the speech to that location.
+func (ps *PollySpeaker) saveAsMP3(fileName string, output *polly.SynthesizeSpeechOutput) error {
+	// Save as MP3
+	names := strings.Split(fileName, ".")
+	name := names[0]
+	mp3File := name + ".mp3"
+
+	outFile, err := os.Create(mp3File)
+	if err != nil {
+		return err
+	}
+
+	defer outFile.Close()
+	_, err = io.Copy(outFile, output.AudioStream)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- pkg/speakers/polly/polly_test.go
+++ pkg/speakers/polly/polly_test.go
@@ -1,1 +1,39 @@
 package polly
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/aws/aws-sdk-go/service/polly"
+)
+
+
+const exampleMP3Path = "pkg/speakers/polly.go/test_files/test.mp3"
+
+func loadExampleFile(t *testing.T) io.ReadCloser {
+	t.Helper()
+	f, err := os.Open(exampleMP3Path)
+	if err != jnil {
+		t.Fatal(err)
+	}
+	return f
+}
+
+func TestPollySpeaker_saveAsMP3(T *testing.T) {
+	T.Parallel()
+
+	ps := New("joanna")
+
+	T.Run("generic", func(t *testing.T) {
+		t.Parallel()
+
+		exampleOutputFilename := "testsave.mp3"
+		exampleData := &polly.SynthesizeSpeechOutput{AudioStream: loadExampleFile(t)}
+
+		err := ps.saveAsMP3(exampleOutputFilename, exampleData)
+		assert.NoError(t, err)
+		assert.NoError(t, os.Remove(exampleOutputFilename))
+	})
+}
